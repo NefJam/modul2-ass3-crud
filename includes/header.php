8da86b8afc547e7c8216ba2550ff28921ce2d5a4
NefJam <mathiasjlarsen@gmail.com>
2016-10-07 09:29:38 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="./css/main.css" />
+<title>modul 2 opgave 3 - Resource type</title>
+</head>
+
+<body>
+
+	<div class="container">
